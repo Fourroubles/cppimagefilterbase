15a549f53b7d2e9c1725ec1b5d0b069c1e6c58aa
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-10-11 02:54:55 +0300

    Add files via upload

--- Filter.cpp
+++ Filter.cpp
@@ -3,16 +3,16 @@
 void RedFilter::RedPainting(image_data &imgData, const WorkFile qq) {
 	int x1 = 0 ,x2 = 0 ,y1=0,y2= 0;
 	if (qq.u != 0) {
-		x1 = imgData.w / qq.u;
+		x1 = imgData.h / qq.u;
 	}
 	if (qq.b != 0) {
-		x2 = imgData.w / qq.b;
+		x2 = imgData.h / qq.b;
 	}
 	if (qq.l != 0) {
-		y1 = imgData.h / qq.l;
+		y1 = imgData.w / qq.l;
 	}
 	if (qq.r != 0) {
-		y2 = imgData.h / qq.r;
+		y2 = imgData.w / qq.r;
 	}
 		size_t img_size = imgData.w *imgData.h;
 		for (int i = y1; i < y2; ++i) {
