1d7a0ef8ff0214a7b48d2ebdd9a35aea97dd29a7
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-10-11 02:33:45 +0300

    Add files via upload

--- Filter.cpp
+++ Filter.cpp
@@ -1,31 +1,27 @@
 #include "Filter.h"
 
 void RedFilter::RedPainting(image_data &imgData, const WorkFile qq) {
-	int x1 = 0,x2 = imgData.w,y1= 0,y2= imgData.h;
-	//if (qq.u != 0) {
-	//	x1 = imgData.w / qq.u;
-	//}
+	int x1 = 0 ,x2 = 0 ,y1=0,y2= 0;
+	if (qq.u != 0) {
+		x1 = imgData.w / qq.u;
+	}
 	if (qq.b != 0) {
 		x2 = imgData.w / qq.b;
 	}
-	/*if (qq.l != 0) {
+	if (qq.l != 0) {
 		y1 = imgData.h / qq.l;
-	}*/
+	}
 	if (qq.r != 0) {
 		y2 = imgData.h / qq.r;
 	}
 		size_t img_size = imgData.w *imgData.h;
-		unsigned char *p = imgData.pixels;
-		p += (y1*imgData.w + x1) * imgData.compPerPixel;
 		for (int i = y1; i < y2; ++i) {
-			for (int j = (i*imgData.w) + x1; j < (i*imgData.w) + x2; ++j) {
-					*(p) = (uint8_t)255;         // red
-					*(p + 1) = (uint8_t)0;
-					*(p + 2) = (uint8_t)0;
-				
-				p += imgData.compPerPixel;
+			for (int j = x1; j < x2; ++j) {
+				int ptr = (i*imgData.w + j)*imgData.compPerPixel;
+					imgData.pixels[ptr + 0] = (uint8_t)255;         // red
+					imgData.pixels[ptr + 1] = (uint8_t)0;
+					imgData.pixels[ptr + 2] = (uint8_t)0;
 			}
-			p += (imgData.w - x1 - x2) * imgData.compPerPixel;
 		}
 	/*	for (int i = 0; i < imgData.w *imgData.h; p += imgData.compPerPixel, ++i) {
 			if(i*imgData.h + imgData.w && i<=x2)
