69fae2f6dc8e8d3fe78f2852fcdcdb64c52d0769
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-10-13 01:47:31 +0300

    Add files via upload

--- WorkFile.cpp
+++ WorkFile.cpp
@@ -2,10 +2,6 @@
 
 void WorkFile::OutPutFile(const std::string &config) {
 	std::ifstream file(config);
-	if (!file) {
-		std::cout << "File not open!";
-		exit(0);
-	}
 	file >> FilterName >> up >> left >> back >> right;
 	file.close();
 }
