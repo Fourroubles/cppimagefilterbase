9bd1c5270e4decc718dfa45c184c966f44c7c060
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-10-15 16:32:28 +0300

    Add files via upload

--- Filter.h
+++ Filter.h
@@ -10,9 +10,9 @@
 
 class Filter {
 public:
-	virtual void RedPainting(image_data &imgData) {};
+	/*virtual void RedPainting(image_data &imgData) {};
 	virtual void BlackWhitePainting(image_data &imgData, std::vector<int> CoordinateUsingFilter) {};
-	virtual void ThresholdPainting(WorkFile ConfigData, image_data &imgData) {};
+	virtual void ThresholdPainting(WorkFile ConfigData, image_data &imgData) {};*/
 
 	int ColcualteUpCoordinate(image_data &imgData, const WorkFile ConfigCoordinate);
 	int ColcualteLeftCoordinate(image_data &imgData, const WorkFile ConfigCoordinate);
@@ -23,24 +23,24 @@ public:
 
 class RedFilter: public Filter {
 public:
-	virtual void RedPainting(image_data &imgData);
+	/*virtual */void RedPainting(image_data &imgData);
 	std::vector<int> CoordinateUsingFilter;
 };
 
-class BlackWhiteFilter: public Filter {
-public:
-	virtual void BlackWhitePainting(image_data &imgData, std::vector<int> CoordinateUsingFilter);
-};
-
-class ThresholdFilter : public Filter {
-public:
-	virtual void ThresholdPainting(WorkFile ConfigData, image_data &imgData);
-	std::vector<int> CoordinateUsingFilter;
-private:
-	bool CalculateMediana(std::vector<int> CoordinateUsingFilter, const image_data &imgData, int i, int j);
-	std::vector<int> Filling—ontainerIntensity(std::vector<int> CoordinateUsingFilter, const image_data &imgData, int  i, int j);
-	void ChangePixel(image_data &imgData, std::vector<int> CoordinateUsingFilter);
-};
+//class BlackWhiteFilter: public Filter {
+//public:
+//	/*virtual*/ void BlackWhitePainting(image_data &imgData, std::vector<int> CoordinateUsingFilter);
+//};
+//
+//class ThresholdFilter : public Filter {
+//public:
+//	/*virtual*/ void ThresholdPainting(WorkFile ConfigData, image_data &imgData);
+//	std::vector<int> CoordinateUsingFilter;
+//private:
+//	bool CalculateMediana(std::vector<int> CoordinateUsingFilter, const image_data &imgData, int i, int j);
+//	std::vector<int> Filling—ontainerIntensity(std::vector<int> CoordinateUsingFilter, const image_data &imgData, int  i, int j);
+//	void ChangePixel(image_data &imgData, std::vector<int> CoordinateUsingFilter);
+//};
 
 class SelectionFilter {
 public:
