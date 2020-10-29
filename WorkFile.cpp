ef74c67a69e15c9775fe59f5ecd76e79ed6a23eb
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-10-29 21:29:34 +0300

    Add files via upload

--- WorkFile.cpp
+++ WorkFile.cpp
@@ -2,6 +2,24 @@
 
 void WorkFile::OutPutFile(const std::string &config) {
 	std::ifstream file(config);
-	file >> FilterName >> up >> left >> back >> right;
+	char *str = new char[150];
+	int k = 0;
+	while (!file.eof()) {
+
+		file.getline(str, 150);
+		
+		for (int i = 0; str[i] != '\n'; ++i) {
+			if (str[i] == ' ') {
+				
+				k = i;
+				break;
+			}
+			FilterName.push_back(str[i]);
+		}
+		up = str[k + 1] - '0';
+		left = str[k + 3] - '0';
+		back = str[k + 5] - '0';
+		right = str[k + 7] - '0';
+	}
 	file.close();
 }
