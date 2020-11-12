729f26eb2f2a3ac925731731d64ca7f10a2d3736
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-11-12 05:03:38 +0300

    Add files via upload

--- Filter.cpp
+++ Filter.cpp
@@ -41,7 +41,7 @@ void Blur::filter(image_data &imgData, std::vector<int> CoordinateUsingFilter) {
 	ManagerFilter* manager = new ManagerBlurFilter();
 	CopyPixel = ManagerFilter::copyImage(imgData);
 	for (int i = CoordinateUsingFilter[0]; i < CoordinateUsingFilter[1]-100; ++i) {
-		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]-100; ++j) {
+		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]-1000; ++j) {
 			int ptr = (i*imgData.w + j)*imgData.compPerPixel;
 			intensity = manager->calculateIntensity(CoordinateUsingFilter, CopyPixel, i, j);
 			imgData.pixels[ptr + 0] = intensity[0];
