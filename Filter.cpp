00abea90554c764b7573adb9c8324c1f69bfd101
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-11-12 04:41:11 +0300

    Add files via upload

--- Filter.cpp
+++ Filter.cpp
@@ -41,12 +41,12 @@ void Blur::filter(image_data &imgData, std::vector<int> CoordinateUsingFilter) {
 	ManagerFilter* manager = new ManagerBlurFilter();
 	CopyPixel = ManagerFilter::copyImage(imgData);
 	for (int i = CoordinateUsingFilter[0]; i < CoordinateUsingFilter[1]; ++i) {
-		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]; ++j) {
+		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]+100; ++j) {
 			int ptr = (i*imgData.w + j)*imgData.compPerPixel;
 			intensity = manager->calculateIntensity(CoordinateUsingFilter, CopyPixel, i, j);
 			imgData.pixels[ptr + 0] = intensity[0];
-			imgData.pixels[ptr + 1] = intensity[0];
-			imgData.pixels[ptr + 2] = intensity[0];
+			imgData.pixels[ptr + 1] = intensity[1];
+			imgData.pixels[ptr + 2] = intensity[2];
 		}
 	}
 }
@@ -55,7 +55,7 @@ void Blur::filter(image_data &imgData, std::vector<int> CoordinateUsingFilter) {
 void Edge::filter(image_data &imgData, std::vector<int> CoordinateUsingFilter) {
 	ManagerFilter* manager = new ManagerEdgeFilter();
 	CopyPixel = ManagerFilter::copyImage(imgData);
-	for (int i = CoordinateUsingFilter[0]-1; i < CoordinateUsingFilter[1]; ++i) {
+	for (int i = CoordinateUsingFilter[0]; i < CoordinateUsingFilter[1]; ++i) {
 		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]; ++j) {
 			int ptr = (i*imgData.w + j)*imgData.compPerPixel;
 			intensity = manager->calculateIntensity(CoordinateUsingFilter, CopyPixel, i, j);
