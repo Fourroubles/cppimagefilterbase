3e696368d6261193d33188d4f1cd7dba8201d65e
Fourroubles <60853520+Fourroubles@users.noreply.github.com>
2020-11-12 04:01:12 +0300

    Add files via upload

--- Filter.cpp
+++ Filter.cpp
@@ -56,7 +56,7 @@ void Edge::filter(image_data &imgData, std::vector<int> CoordinateUsingFilter) {
 	ManagerFilter* manager = new ManagerEdgeFilter();
 	CopyPixel = ManagerFilter::copyImage(imgData);
 	for (int i = CoordinateUsingFilter[0]; i < CoordinateUsingFilter[1]; ++i) {
-		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]; ++j) {
+		for (int j = CoordinateUsingFilter[2]; j < CoordinateUsingFilter[3]+100; ++j) {
 			int ptr = (i*imgData.w + j)*imgData.compPerPixel;
 			intensity = manager->calculateIntensity(CoordinateUsingFilter, CopyPixel, i, j);
 			imgData.pixels[ptr + 0] = imgData.pixels[ptr + 1] = imgData.pixels[ptr + 2] = (unsigned char)intensity[0];
